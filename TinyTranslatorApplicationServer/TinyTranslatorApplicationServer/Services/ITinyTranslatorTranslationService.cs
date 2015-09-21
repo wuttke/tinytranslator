85824bc7b9f5ed1593bc570a3c1955e6a3366e1f
Matthias Wuttke <matthias@wuttke.eu>
2015-09-21 19:46:22 +0200

    new methods (unimplemented)

--- TinyTranslatorApplicationServer/TinyTranslatorApplicationServer/Services/ITinyTranslatorTranslationService.cs
+++ TinyTranslatorApplicationServer/TinyTranslatorApplicationServer/Services/ITinyTranslatorTranslationService.cs
@@ -12,6 +12,31 @@ namespace TinyTranslatorApplicationServer.Services
     public interface ITinyTranslatorTranslationService
     {
 
+        /* TODO
+        /// <summary>
+        /// Gets all projects.
+        /// </summary>
+        [OperationContract]
+        ICollection<Project> GetProjects();
+
+        /// <summary>
+        /// Gets all assemblies of the project.
+        /// </summary>
+        /// <param name="projectID"></param>
+        /// <returns></returns>
+        [OperationContract]
+        ICollection<ResourceAssembly> GetAssemblies(int projectID);
+
+        [OperationContract]
+        ICollection<ResourceBundle> GetAllBundles(int projectID);
+
+        [OperationContract]
+        ICollection<ResourceBundle> GetAssemblyBundles(int projectID, int assemblyID);
+        */
+
+        /// <summary>
+        /// Gets a resource bundle with all resources and translations.
+        /// </summary>
         [OperationContract]
         ResourceBundle GetResourceBundle(int bundleID);
 
